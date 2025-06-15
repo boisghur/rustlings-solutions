6e735d1b9d3d45c3551c7f6abe781300281f9544
boisghur <74437002+boisghur@users.noreply.github.com>
2025-06-15 02:37:46 -0400

    Modules + Hashmaps done

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,8 +31,31 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
-    }
-
+        if let Some(team) = scores.get_mut(team_1_name){
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        } else {
+            scores.insert(
+                team_1_name,
+                TeamScores{
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                }
+            );
+        };
+        if let Some(team) = scores.get_mut(team_2_name){
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        } else {
+            scores.insert(
+                team_2_name,
+                TeamScores{
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                }
+            );
+        };
+    };
     scores
 }
 
